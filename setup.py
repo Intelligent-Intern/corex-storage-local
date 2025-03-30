bad49743b5db07fc7bf458f53a601a6bbbb45ec0
Jochen Schultz <jschultz@php.net>
2025-03-30 16:25:27 +0200

    Initial commit with GitHub Action workflow

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="corex-storage-local",
+    version="0.1.0",
+    author="Jochen Schultz",
+    author_email="js@intelligent-intern.com",
+    packages=["corex_storage_local"],
+    install_requires=[],
+    python_requires=">=3.7",
+)
